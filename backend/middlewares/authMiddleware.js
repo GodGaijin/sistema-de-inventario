e08bdc1be939bc402b8cf42cc9917292b776be89
Juan Portillo <juanportillo.0509@gmail.com>
2025-07-20 22:52:05 -0400

    hotfix Belgium 1940

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -10,7 +10,7 @@ const authenticateToken = (req, res, next) => {
   const token = authHeader && authHeader.split(' ')[1];
   if (!token) return res.sendStatus(401);
   jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) return res.sendStatus(401); // Cambiar de 403 a 401 para tokens inválidos
     req.user = user;
     next();
   });
